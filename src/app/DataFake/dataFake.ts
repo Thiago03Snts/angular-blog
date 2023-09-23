8428c53b11aacb4d30e0752dc715bba629c45092
Thiago03Snts <dev.thiagoalves@gmail.com>
2023-09-23 20:25:10 -0300

    new content updated!!!

--- src/app/DataFake/dataFake.ts
+++ src/app/DataFake/dataFake.ts
@@ -8,8 +8,25 @@ export const dataFake = [
 
   {
     "id": "2",
-    "title": "saiu novo filme",
-    "description": "Olá",
+    "title": "RESISTÊCIA CONQUISTA TERRITÓRIOS DO IMPÉRIO!",
+    "description": "A resistênia conta com apoio Jedi para recuperar territórios importantes.",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-_EfTdw_GtvRPEAkZNxJZue1zIPxHca5e8A&usqp=CAU"
-  }
+  },
+
+  {
+    "id": "3",
+    "title": "STAR WARS: Ahsoka",
+    "description": "Nova série do universo Star Wars chega ao Disney Plus",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtljxbIXPK9aDRiTN3K4CfN1o2BXpmEhlJCg&usqp=CAU"
+  },
+  
+  {
+    "id": "4",
+    "title": "STAR WARS: The Mandalorian",
+    "description": "Nova temporada começa com tudo!",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6a7MQ6DhkEiDkJKrVYbLGJRUhBILRs8j2pg&usqp=CAU"
+  },
+
+
 ]
+
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -27,12 +27,12 @@ export class ContentComponent implements OnInit {
     this.route.paramMap.subscribe( value =>
       this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
   }
 
   setValuesToComponent(id:string | null){
-    const result = dataFake.filter((article: { id: string | null; }) =>{
-      return article.id == id;
-    })[0]  
+    const result = dataFake.filter(article => article.id == id)[0]  
     this.contentTitle = result.title
     this.contentDescription = result.description
     this.photoCover = result.photoCover
