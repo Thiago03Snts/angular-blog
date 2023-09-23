294e936125e580e4ccffdf13acc9e6491c586bd0
Thiago03Snts <dev.thiagoalves@gmail.com>
2023-09-22 21:41:28 -0300

    content created!

---
+++ src/app/DataFake/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+    "id": "1",
+    "title": "IMPÉRIO AVANÇA SOBRE A AMEAÇA REBELDE",
+    "description": "Forças do Império impedem crescimento da armada rebelde.",
+    "photoCover": "https://www.aereo.jor.br/wp-content/uploads/2010/05/Star-Wars-The-Empire-Strikes-Back-2.jpg"
+  },
+
+  {
+    "id": "2",
+    "title": "saiu novo filme",
+    "description": "Olá",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-_EfTdw_GtvRPEAkZNxJZue1zIPxHca5e8A&usqp=CAU"
+  }
+]
